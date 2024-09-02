3d7cc0b7ee41b3f6a6b5d543c2e14fc5c5d7a373
e7gx <abalgabo@gmail.com>
2024-09-02 13:33:25 +0300

    .

--- Swift Language/While & Repeat Loops/While & Repeat Loops.playground/Contents.swift
+++ Swift Language/While & Repeat Loops/While & Repeat Loops.playground/Contents.swift
@@ -1,17 +1,26 @@
+// for i in 1...5 {
+//     print(i)
+// }
 
 
-for i in 1...5 {
-//    print(i)
-}
+// var count = 1
+// while count < 6 {
+//     print(count)
+//     count += 1 
+// }
 
-var count = 0
-while count > 0 {
-    print(count)
-    count -= 1
-}
+// var i = 6
+// repeat {
+//     print(i)
+//     i += 1
+// }while i < 6 
 
-var i = 10
-repeat {
-//    print(i)
-    i += 1
-}while i < 6
+// for i in 1...7{
+//     for j in 1...5{
+//             if i %  2 == 0 {
+//                 if j % 2 == 0 {
+//                      print("i = \(i) , j = \(j)")
+//                 }
+//             }
+//     }
+// }
